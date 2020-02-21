02ffad90c5abde38003a13d2b8a7a512cf3dbcbb
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-02-21 15:00:47 -0300

    refactor controller

--- appProduct/js/app.js
+++ appProduct/js/app.js
@@ -1,7 +1,27 @@
-var app = angular.module("produtoApp", []);
+var app = angular.module("produtoApp", ["ngRoute"]);
 
-app.controller("ProdutosController", function($scope, ProdutosService) {
-  clear();
+//Rotas da aplicação
+app.config(function($routeProvider) {
+  $routeProvider
+    .when("/cadProduct", {
+      templateUrl: "views/products/insert.html",
+      controller: "InsertController"
+    })
+    .when("/cadProduct/:id", {
+      templateUrl: "views/products/insert.html",
+      controller: "InsertController"
+    })
+    .when("/list", {
+      templateUrl: "views/products/list.html",
+      controller: "listController"
+    })
+    //Rota default
+    .otherwise({
+      redirectTo: "/list"
+    });
+});
+
+app.controller("listController", function($scope, ProdutosService) {
   list();
 
   function list() {
@@ -10,33 +30,54 @@ app.controller("ProdutosController", function($scope, ProdutosService) {
     });
   }
 
-  //save
-  $scope.save = function(produto) {
-    ProdutosService.save(produto).then(list);
-    clear();
-  };
-
   //delete
   $scope.delete = function(produto) {
     ProdutosService.delete(produto).then(list);
-    clear();
   };
 
   //edit
-  $scope.edit = function(produto) {
-    $scope.produto = angular.copy(produto);
+  //$scope.edit = function(produto) {
+  // $scope.produto = angular.copy(produto);
+  //};
+});
+
+app.controller("InsertController", function(
+  $routeParams,
+  $scope,
+  $location,
+  ProdutosService
+) {
+  let id = $routeParams.id;
+  if (id) {
+    ProdutosService.findById(id).then(function(response) {
+      $scope.produto = response.data;
+    });
+  } else {
+    clear();
+  }
+
+  $scope.save = function(produto) {
+    clear();
+    ProdutosService.save(produto).then(redirectList);
   };
 
-  $scope.cancel = function() {
-    clear();
-  };
+  $scope.cancel = redirectList;
+
   function clear() {
     $scope.produto = {};
+  }
+
+  function redirectList() {
+    $location.path("/list");
   }
 });
 
 app.service("ProdutosService", function($http) {
   let baseUrl = "http://192.168.1.110:8080/appServer/api/products";
+
+  this.findById = function(id) {
+    return $http.get(baseUrl + "/" + id);
+  };
 
   this.list = function() {
     return $http.get(baseUrl + "/list");
